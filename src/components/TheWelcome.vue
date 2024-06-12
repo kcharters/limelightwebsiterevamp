789a87effae01effeb75178d556e0cbf92664595
Kirstin Charters <kirstin.charters@otago.ac.nz>
2024-06-13 08:19:17 +1200

    added counter to site

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -5,9 +5,12 @@ import ToolingIcon from './icons/IconTooling.vue'
 import EcosystemIcon from './icons/IconEcosystem.vue'
 import CommunityIcon from './icons/IconCommunity.vue'
 import SupportIcon from './icons/IconSupport.vue'
+
 </script>
 
 <template>
 <h1>Limelight SMP</h1>
 <h2>is currently under construction</h2>
+<h3>We will be live in:</h3>
+<iframe src="https://free.timeanddate.com/countdown/i9ewfh4e/cf12/cm0/cu4/ct0/cs0/ca0/co0/cr0/ss0/caceee/cpceee/pct/tcfff/fs100/szw576/szh243/iso2024-08-14T00:00:00" allowtransparency="true" frameborder="0" width="197" height="52"></iframe>
 </template>
