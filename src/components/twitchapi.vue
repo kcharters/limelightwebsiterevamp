1feb68c0634d542f883ccc65e0f2bff37894dbbe
Kirstin Charters <kirstin.charters@otago.ac.nz>
2024-06-21 15:18:19 +1200

    adding axios for api calls

---
+++ src/components/twitchapi.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+async function fetchTwitchData() {
+        const catFactsResponse = await axios.get('https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=5')
+        catFacts.value = catFactsResponse.data
+    }
+
+
+onMounted(async () => {
+        await fetchTwitchData()
+    })
+</script>
+
+<template>
+
+
+</template>
+
+<style>
+</style>
