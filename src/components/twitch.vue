1d532813c1e8ca2a35b93a77b0e3e5863cea331e
Kirstin Charters <kirstin.charters@otago.ac.nz>
2024-07-06 10:21:01 +1200

    js

---
+++ src/components/twitch.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+    name: 'twitch',
+    data: function () {
+        return {
+            whoIsLive: [],
+        }
+    },
+    methods: {
+        fetchToken: function () {
+            const client_id = import.meta.env.CLIENT_ID;
+            const client_secret = import.meta.env.CLIENT_SECRET;
+            let url = "https://id.twitch.tv/oauth2/token"
+            fetch(url, {
+                method: 'post',
+                headers: {
+                    'Accept': 'application/vnd.twitchtv.v5+json',
+                    'Content-Type': 'x-www-form-urlencoded',
+                },
+                body: {
+                    'client_id': client_id,
+                    'client_secret': client_secret,
+                    "grant_type": 'client_credentials'
+                }
+            }).then(res => {
+                console.log("Token generated => " + res);
+                // this.twitchAccessToken = res.data.access_token;
+            })
+        }
+
+    },
+    mounted() {
+        this.fetchToken()
+    }
+}
+</script>
+<template>
+
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,11 @@
 <script setup>
 import Nav from "../components/nav.vue";
 import Hero from "../components/hero.vue";
-import Twitch from "../components/twitchapi.vue";
+import Twitch from "../components/twitch.vue"
+
 </script>
 <template>
   <Nav />
   <Hero />
-  <Twitch />
+  <Twitch/>
 </template>
